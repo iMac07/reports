4331948e1ed426af0c8bcd37916b82c1b82266e7
unclejo <76188409+unclejo@users.noreply.github.com>
2022-12-25 13:16:46 +0800

    20221225

--- src/org/xersys/reports/BranchInventory1.java
+++ src/org/xersys/reports/BranchInventory1.java
@@ -1,28 +1,19 @@
 package org.xersys.reports;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRResultSetDataSource;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.data.JsonDataSource;
-import net.sf.jasperreports.view.JasperViewer;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.xersys.commander.iface.XNautilus;
 import org.xersys.commander.iface.XReport;
 import org.xersys.commander.util.SQLUtil;
 
-public class BranchInventory implements XReport{
+public class BranchInventory1 implements XReport{
     private final String REPORTID = "220004";
     private final String REPORT_PATH = "/reports/";
     
@@ -35,7 +26,7 @@ public class BranchInventory implements XReport{
     private JasperPrint _jrprint;
     private LinkedList _rptparam = null;
     
-    public BranchInventory(){
+    public BranchInventory1(){
         _rptparam = new LinkedList();
         _rptparam.add("store.report.id");
         _rptparam.add("store.report.no");
@@ -163,7 +154,7 @@ public class BranchInventory implements XReport{
         ResultSet rs = p_oNautilus.executeQuery(getReportSQL());
         
         //Convert the data-source to JasperReport data-source
-        //JRResultSetDataSource jrRS = new JRResultSetDataSource(rs);
+        JRResultSetDataSource jrRS = new JRResultSetDataSource(rs);
         
         //Create the parameter
         Map<String, Object> params = new HashMap<>();
@@ -174,49 +165,12 @@ public class BranchInventory implements XReport{
         params.put("sPrintdBy", (String) p_oNautilus.getUserInfo("xClientNm"));
         
         try {
-//            _jrprint = JasperFillManager.fillReport((String) p_oNautilus.getAppConfig("sApplPath") + REPORT_PATH +
-//                                                    System.getProperty("store.report.file"),
-//                                                    params, 
-//                                                    jrRS);
-            JSONArray json_arr = new JSONArray();
-            json_arr.clear();
-            
-            JSONObject json_obj;
-            
-            int lnCtr = 1;
-            
-            while (rs.next()){
-                json_obj = new JSONObject();
-                
-                json_obj.put("nField01", lnCtr);
-                json_obj.put("sField01", rs.getString("sField01").toUpperCase());
-                json_obj.put("sField02", rs.getString("sField02").toUpperCase());
-                json_obj.put("sField03", rs.getString("sField03").toUpperCase());
-                json_obj.put("sField04", rs.getString("sField04").toUpperCase());
-                json_obj.put("sField05", rs.getString("sField05").toUpperCase());
-                json_obj.put("sField06", rs.getString("sField06").toUpperCase());
-                json_obj.put("nField02", rs.getInt("nField02"));
-                json_obj.put("lField01", rs.getDouble("lField01"));
-                json_obj.put("lField02", rs.getDouble("lField02"));
-                json_obj.put("nField03", rs.getInt("nField03"));
-                json_obj.put("lField03", rs.getDouble("lField03"));
-                
-                lnCtr++;
-                json_arr.add(json_obj); 
-            }
-            
-            InputStream stream = new ByteArrayInputStream(json_arr.toJSONString().getBytes("UTF-8"));
-            JsonDataSource jrjson = new JsonDataSource(stream); 
-
-            _jrprint = JasperFillManager.fillReport(
-                            (String) p_oNautilus.getAppConfig("sApplPath") + REPORT_PATH +
-                            System.getProperty("store.report.file"), params, jrjson);
-            
-            //JasperViewer jv = new JasperViewer(_jrprint, false);
-            //jv.setVisible(true);
-            
-        } catch (SQLException | JRException | UnsupportedEncodingException ex) {
-            ex.printStackTrace();        
+            _jrprint = JasperFillManager.fillReport((String) p_oNautilus.getAppConfig("sApplPath") + REPORT_PATH +
+                                                    System.getProperty("store.report.file"),
+                                                    params, 
+                                                    jrRS);
+        } catch (JRException ex) {
+            ex.printStackTrace();
         }
         
         return _jrprint;
@@ -227,14 +181,12 @@ public class BranchInventory implements XReport{
                     "  b.sBarCodex sField01" +
                     ", b.sDescript sField02" +
                     ", IFNULL(c.sDescript, '') sField03" +
-                    ", 'MODEL' sField04" +
-                    ", 'SIZE' sField05" +
-                    ", 'OUM' sField06" +
-                    ", a.nQtyOnHnd nField02" +
+                    ", a.cClassify sField04" +
+                    ", a.nMaxLevel nField01" +
+                    ", a.nMinLevel nField02" +
                     ", b.nSelPrce1 lField01" +
-                    ", a.nQtyOnHnd * b.nSelPrce1 lField02" +
-                    ", 0 nField03" +
-                    ", b.nUnitPrce lField03" +	
+                    ", a.nQtyOnHnd nField03" +
+                    ", b.nUnitPrce lField02" +	
                 " FROM Inv_Master a" +
                     ", Inventory b" +
                     " LEFT JOIN Brand c ON b.sBrandCde = c.sBrandCde AND c.sInvTypCd = 'SP'" +
@@ -242,7 +194,7 @@ public class BranchInventory implements XReport{
                     " AND a.sBranchCd = " + SQLUtil.toSQL((String) p_oNautilus.getBranchConfig("sBranchCd")) +
                     " AND a.cRecdStat = '1'" +
                     " AND a.nQtyOnHnd > 0" +
-                " ORDER BY b.sBrandCde, b.sBarCodex, b.sDescript, a.nQtyOnHnd";
+                " ORDER BY b.sBrandCde, c.sDescript, a.nQtyOnHnd";
     }
 
     @Override
